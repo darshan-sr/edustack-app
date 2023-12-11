d4499a531dc47f1a1fff050a624d270be580fb3a
Darshan Gowda <96766940+darshan-sr@users.noreply.github.com>
2023-12-12 01:27:49 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -46,44 +46,49 @@ const Home = () => {
       router.push(url.url);
     }
   }, []);
-
-  const storedInstitution = localStorage.getItem("selectedInstitution");
-  if (!storedInstitution) {
-    return (
-      <div className="min-w-screen min-h-screen flex flex-col items-center justify-center">
-        <div className="w-11/12 max-w-md flex flex-col items-center justify-center text-center bg-white rounded-xl border border-solid border-gray-200 p-4">
-          <h4 className="font-poppins flex flex-row  my-4 font-semibold  text-[25px] text-gray-700 mt-3">
-            <BsStack className="w-10 h-10 text-blue-600 pr-2" /> Edustack
-          </h4>
-          <p>Welcome, Please select your Institution to Continue!</p>
-          <Select
-            placeholder="Select your institution"
-            size="large"
-            style={{ width: "100%", marginBottom: 30, marginTop: 30 }}
-            onChange={(value) =>
-              setSelectedInstitution(
-                institutions.find((inst) => inst.name === value) as Institution
-              )
-            }
-          >
-            {institutions.map((inst) => (
-              <Option key={inst.name} value={inst.name}>
-                {inst.name}
-              </Option>
-            ))}
-          </Select>
-          <Button
-            type="primary"
-            className="bg-blue-600"
-            size="large"
-            block
-            onClick={handleContinue}
-          >
-            Continue
-          </Button>
+  if (typeof window !== "undefined") {
+    const storedInstitution = localStorage.getItem("selectedInstitution");
+    if (!storedInstitution) {
+      return (
+        <div className="min-w-screen min-h-screen flex flex-col items-center justify-center">
+          <div className="w-11/12 max-w-md flex flex-col items-center justify-center text-center bg-white rounded-xl border border-solid border-gray-200 p-4">
+            <h4 className="font-poppins flex flex-row  my-4 font-semibold  text-[25px] text-gray-700 mt-3">
+              <BsStack className="w-10 h-10 text-blue-600 pr-2" /> Edustack
+            </h4>
+            <p>Welcome, Please select your Institution to Continue!</p>
+            <Select
+              placeholder="Select your institution"
+              size="large"
+              style={{ width: "100%", marginBottom: 30, marginTop: 30 }}
+              onChange={(value) =>
+                setSelectedInstitution(
+                  institutions.find(
+                    (inst) => inst.name === value
+                  ) as Institution
+                )
+              }
+            >
+              {institutions.map((inst) => (
+                <Option key={inst.name} value={inst.name}>
+                  {inst.name}
+                </Option>
+              ))}
+            </Select>
+            <Button
+              type="primary"
+              className="bg-blue-600"
+              size="large"
+              block
+              onClick={handleContinue}
+            >
+              Continue
+            </Button>
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return <></>;
+    }
   } else {
     return <></>;
   }
